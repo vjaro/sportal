5368a6c1ce2a09c90f1880ffea8aed9ab4209892
vjaro <vincent.jaro@devtac.asia>
2020-07-09 19:33:22 +0800

    added review auth component

--- src/app/pages/penalty-system/penalty-system.module.ts
+++ src/app/pages/penalty-system/penalty-system.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { NbCardModule } from '@nebular/theme';
 import { NbStepperModule } from '@nebular/theme';
+import { NbRadioModule} from '@nebular/theme';
+import { NbInputModule} from '@nebular/theme';
+import { NbButtonModule } from '@nebular/theme';
+import { NbDatepickerModule } from '@nebular/theme';
+import { NbSelectModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { PenaltySystemComponent } from './penalty-system.component';
 import { TradeFlowComponent } from './trade-flow/trade-flow.component';
+import { ReviewAuthComponent } from './review-auth/review-auth.component';
 
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
     NbStepperModule,
+    NbRadioModule,
+    NbInputModule,
+    NbButtonModule,
+    NbDatepickerModule,
+    NbSelectModule,
   ],
   declarations: [
     PenaltySystemComponent,
     TradeFlowComponent,
+    ReviewAuthComponent,
   ],
 
 })
---
+++ src/app/pages/penalty-system/review-auth/review-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-review-auth',
+  templateUrl: './review-auth.component.html',
+  styleUrls: ['./review-auth.component.scss'],
+})
+export class ReviewAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
